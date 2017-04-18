66f7bb59dacaf7b27644939ed3edd778eeeb2a01
sf <lazy.lizard@yandex.ru>
2017-04-18 19:04:23 +0300

    connect app to store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ManufsAndTypes from '../components/manufsAndTypes'
 //import { bindActionCreators } from 'redux'
-//import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 
 class App extends Component {
 
@@ -13,7 +13,18 @@ class App extends Component {
             <ManufsAndTypes mfList={{a:1}}/>
         </div>
     }
+
+
 }
 
-export default App
+function mapStateToProps(state) {
+    return {
+        ml: state.ManufAndTypesList
+    }
+
+
+}
+
+//export default App
+export default connect(mapStateToProps)(App)
 
