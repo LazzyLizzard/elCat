634933c0860ad6f2928be9cd0d3da1837e3aaa0b
sf <lazy.lizard@yandex.ru>
2017-04-24 13:12:50 +0300

    href example

--- src/components/TransportTypes.js
+++ src/components/TransportTypes.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 
 class TransportTypes extends Component {
 
-    //constructor(props) {
-    //    super(props);
-    //}
-
-
     onTransportTypeClick(manufId, transportTypeId) {
         console.log('click, m: %s, t: %s, ', manufId, transportTypeId);
         event.preventDefault();
@@ -16,20 +11,20 @@ class TransportTypes extends Component {
     render() {
         //let trReady = {};
         const trRaw = this.props.transportTypesRaw;
-        const transportTypesData = this.props.transportTypesData;
+        //const transportTypesData = this.props.transportTypesData;
         const manufId = this.props.manufId;
 
-        console.log(transportTypesData);
+        //console.log(transportTypesData);
 
         return (
             <ul>
 
                 {trRaw.map((transportTypeId) => {
                     return <li><a
+                        //href={'#' + manufId}
                         href='#'
                         data-manuf-id={manufId}
                         data-transport-id={transportTypeId}
-                        //onClick={this.onTransportTypeClick.bind(this)}
                         onClick={() => this.onTransportTypeClick(manufId, transportTypeId)}
                         key={transportTypeId}>
                         --- transportTypeId {transportTypeId}, manufId {manufId}
