b0a30282224a2ab682c106d7c8ef66902834bd96
sf <lazy.lizard@yandex.ru>
2017-04-26 10:59:48 +0300

    stable, non-functional

---
+++ src/store/_ttypes.js
@@ -0,0 +1,23 @@
+var x = {
+    "manufList": [
+        {
+            "manuf_id": "1",
+            "manuf_name": "HONDA",
+            "manuf_name_safe": ""
+        },
+        {
+            "manuf_id": "2",
+            "manuf_name": "KAWASAKI",
+            "manuf_name_safe": ""
+        },
+        {
+            "manuf_id": "3",
+            "manuf_name": "SUZUKI",
+            "manuf_name_safe": ""
+        }, {
+            "manuf_id": "4",
+            "manuf_name": "YAMAHA",
+            "manuf_name_safe": ""
+        }],
+    "manufNumber": 4
+}
