c27531a498931b054f552f4bf25c0b8463c4e8f1
Sergei Frolov1 <sergei_frolov1@epam.com>
2017-05-19 16:06:23 +0300

    routing

---
+++ src/routes/index.js
@@ -0,0 +1,39 @@
+export default routes = {
+    path: '/',
+    component: Layout,
+    indexRoute: {
+        component: () => {
+            return (
+                <div></div>
+            )
+        }
+    },
+    childRoutes: [
+        {
+            path: ROUTE.DASHBOARD,
+            component: Dashboard
+        },
+        {
+            path: ROUTE.PAYMENTS,
+            component: Payments
+        },
+        {
+            path: ROUTE.DEAL_PASSPORTS_CONTRACT,
+            component: DealPassportsContractPage,
+            headerText: PAGE_HEADERS.DEAL_PASSPORTS_CONTRACT_HEADER
+        },
+        {
+            path: `${ROUTE.DEAL_PASSPORTS_CONTRACT}/:id`,
+            component: DealPassportContract
+        },
+        {
+            path: ROUTE.NEW_DEAL_PASSPORT_CONTRACT,
+            component: DealPassportContract
+        },
+        {
+            path: BANK_EMPLOYEE_ROUTE.DEAL_PASSPORTS_CONTRACT,
+            component: BankEmployeeDealPassportsContractPage,
+            headerText: PAGE_HEADERS.BANK_EMPLOYEE_DEAL_PASSPORTS_CONTRACT_HEADER
+        }
+    ]
+}
