9b128f26538c750346cf9a13310eee1febdce8be
Sergei Frolov1 <sergei_frolov1@epam.com>
2017-06-01 14:19:31 +0300

    unstable routes with hash
    Issue: MSBG-73

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,64 +1,59 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+import { Route, IndexRoute } from 'react-router'
 
-import * as C from './constants'
+import App from './../containers/App'
+import Home from './../components/Home';
+import MakeYear from './../components/MakelYear';
+import MakeYearTransport from './../components/MakeYearTransport';
+import NotFound from './../components/NotFound';
 
-import App from './containers/App'
+export const routes = (
+    <div>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home}/>
+            <Route path='my/:make/:year' component={MakeYear}/>
+            <Route path='myt/:make/:year/:transport' component={MakeYearTransport}/>
+        </Route>
+        <Route path='*' component={NotFound} />
+    </div>
+);
 
-import Home from './components/Home';
-import MakeYear from './components/MakelYear';
-import MakeYearTransport from './components/MakeYearTransport';
-
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-
-const ROUTE = {
-    my: {}
-};
-
-<Route path='/' component={Home}>
-    <IndexRoute component={App} />
-    <Route path='my' component={MakeYear} />
-    <Route path='myt' component={MakeYearTransport} />
-</Route>
-
-
-export const routes = {
-    path: `${C.RELATED}/`,
-    component: Layout,
-    indexRoute: {
-        component: () => {
-            return (
-                <div>*</div>
-            )
-        }
-    },
-    childRoutes: [
-        {
-            path: ROUTE.my,
-            component: null
-        },
-        {
-            path: ROUTE.PAYMENTS,
-            component: Payments
-        },
-        {
-            path: ROUTE.DEAL_PASSPORTS_CONTRACT,
-            component: DealPassportsContractPage,
-            headerText: PAGE_HEADERS.DEAL_PASSPORTS_CONTRACT_HEADER
-        },
-        {
-            path: `${ROUTE.DEAL_PASSPORTS_CONTRACT}/:id`,
-            component: DealPassportContract
-        },
-        {
-            path: ROUTE.NEW_DEAL_PASSPORT_CONTRACT,
-            component: DealPassportContract
-        },
-        {
-            path: BANK_EMPLOYEE_ROUTE.DEAL_PASSPORTS_CONTRACT,
-            component: BankEmployeeDealPassportsContractPage,
-            headerText: PAGE_HEADERS.BANK_EMPLOYEE_DEAL_PASSPORTS_CONTRACT_HEADER
-        }
-    ]
-}
+//export const routes = {
+//    path: `${C.RELATED}/`,
+//    component: Layout,
+//    indexRoute: {
+//        component: () => {
+//            return (
+//                <div>*</div>
+//            )
+//        }
+//    },
+//    childRoutes: [
+//        {
+//            path: ROUTE.my,
+//            component: null
+//        },
+//        {
+//            path: ROUTE.PAYMENTS,
+//            component: Payments
+//        },
+//        {
+//            path: ROUTE.DEAL_PASSPORTS_CONTRACT,
+//            component: DealPassportsContractPage,
+//            headerText: PAGE_HEADERS.DEAL_PASSPORTS_CONTRACT_HEADER
+//        },
+//        {
+//            path: `${ROUTE.DEAL_PASSPORTS_CONTRACT}/:id`,
+//            component: DealPassportContract
+//        },
+//        {
+//            path: ROUTE.NEW_DEAL_PASSPORT_CONTRACT,
+//            component: DealPassportContract
+//        },
+//        {
+//            path: BANK_EMPLOYEE_ROUTE.DEAL_PASSPORTS_CONTRACT,
+//            component: BankEmployeeDealPassportsContractPage,
+//            headerText: PAGE_HEADERS.BANK_EMPLOYEE_DEAL_PASSPORTS_CONTRACT_HEADER
+//        }
+//    ]
+//}
