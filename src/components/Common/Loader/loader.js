827c9297650bb5e43ecd6daeb9a3e5fcce83761a
Sergei Frolov1 <sergei_frolov1@epam.com>
2017-06-29 17:59:23 +0300

    loader component
    Issue: MSBG-73

---
+++ src/components/Common/Loader/loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Loader({
+    model,
+    visible
+}) {
+
+    const vs = visible ? 'block' : 'none';
+    return (
+        <div style={{display: vs}}>
+            `LOADING ${model}`
+        </div>
+    );
+}
