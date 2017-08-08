64a64fd99745c78adfe6eaeef7034433b00b181a
Sergei Frolov1 <sergei_frolov1@epam.com>
2017-08-08 18:28:27 +0300

    MSBG-73 lil style

--- src/components/MakelYear/index.js
+++ src/components/MakelYear/index.js
@@ -2,17 +2,16 @@ import React, {PropTypes, Component} from 'react';
 import {Link} from 'react-router';
 
 class MakeYear extends Component {
-
     render() {
         return (
             <div>
-                <div><Link to='/'>home</Link></div>
+                <div><Link to="/">home</Link></div>
 
                 <h4>make year</h4>
 
                 honda &nbsp;
-                <Link to='/myt/1/1990/'>1990</Link> &nbsp;
-                <Link to='/myt/1/1991/'>1991</Link>
+                <Link to="/myt/1/1990/">1990</Link> &nbsp;
+                <Link to="/myt/1/1991/">1991</Link>
 
             </div>
         );
