6c39d6acb3393811924cf67468ef58f59a92ed79
Sergei Frolov1 <sergei_frolov1@epam.com>
2017-08-08 18:28:27 +0300

    MSBG-73 merge
    Signed-off-by: Sergei Frolov1 <sergei_frolov1@epam.com>

---
+++ src/routes/routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+import App from './../containers/App';
+import Home from '../components/Home/home';
+import MakeYear from './../components/MakelYear';
+import MakeType from './../components/MakeType';
+import MakeYearTransport from './../components/MakeYearTransport';
+import Model from './../components/Model';
+import NotFound from './../components/not-found';
+
+export default function RoutesList() {
+    return (
+        <div>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home} />
+                <Route path="/mt/:make/:typeId" component={MakeType} />
+                <Route path="/my/:make/:year" component={MakeYear} />
+                <Route
+                    path="/myt/:make/:year/:transport"
+                    component={MakeYearTransport}
+                />
+                <Route path="/model/:modelId" component={Model} />
+            </Route>
+            <Route path="*" component={NotFound} />
+        </div>
+    );
+}
