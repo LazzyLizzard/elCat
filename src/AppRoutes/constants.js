5f4f943cf4758e6338405f0752a2a0d81a73f4c4
SF <lazy.lizard@yandex.ru>
2017-10-05 19:13:01 +0300

    MSBG-75
     * draft view toggler

---
+++ src/AppRoutes/constants.js
@@ -0,0 +1,16 @@
+
+const LOCAL_HTTPS = 'LOCAL_HTTPS';
+const REMOTE_HTTPS = 'REMOTE_HTTPS';
+
+export const ENV = {
+    [LOCAL_HTTPS]: {
+        protocol: 'https://',
+        host: 'localhost'
+    },
+    [REMOTE_HTTPS]: {
+        protocol: 'https://',
+        host: 'motosnab.ru'
+    }
+};
+
+const getRequestEnvironment = env => (ENV[env]);
