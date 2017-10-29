72a12cf9e8377f8f2fb9019e18a787667bf09f18
git <lazy.lizard@yandex.ru>
2017-10-30 01:07:34 +0300

    MSBG-81: Routing для кастомера
    * NA

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
-import {App} from '../containers';
-import {Home} from '../components/Home';
-import {FicheHome} from '../pages/fiche/home';
-import {MakeType} from '../pages/fiche/make-type';
-import {MakeYear} from '../pages/fiche/make-year';
-import {MakeYearTransport} from '../pages/fiche/make-year-transport';
-import {Model} from '../pages/fiche/model';
-import {GroupDetails} from '../pages/fiche/group-details';
-import {NotFound} from '../components/not-found/not-found';
-import {Cart} from '../pages/cart';
-import {NAMESPACE as FICHE_NAMESPACE} from '../pages/fiche/model/reducer';
-import {NAMESPACE as CART_NAMESPACE} from '../pages/cart/reducer';
+import {App} from 'containers';
+import {Home} from 'components/Home';
+import {FicheHome} from 'pages/fiche/home';
+import {MakeType} from 'pages/fiche/make-type';
+import {MakeYear} from 'pages/fiche/make-year';
+import {MakeYearTransport} from 'pages/fiche/make-year-transport';
+import {Model} from 'pages/fiche/model';
+import {GroupDetails} from 'pages/fiche/group-details';
+import {NotFound} from 'components/not-found/not-found';
+import {Cart} from 'pages/cart';
+import {NAMESPACE as FICHE_NAMESPACE} from 'pages/fiche/model/reducer';
+import {NAMESPACE as CART_NAMESPACE} from 'pages/cart/reducer';
 
 export default (
     <div>
