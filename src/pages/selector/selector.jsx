3a5330c1c927c494f9f9d5a7db7c16999a58b7ba
SF <lazy.lizard@yandex.ru>
2017-11-08 19:16:21 +0300

    MSBG-89: FE Develop form and parse query string
    * test form, raw

--- src/pages/selector/selector.jsx
+++ src/pages/selector/selector.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import TestForm from './_form';
+// import TestForm from './_form';
+import {reduxForm} from 'redux-form';
+import CheckboxGroup from './cba';
+import {FORM_DATA} from './test-data';
 
-export class Selector extends React.Component {
+class Selector extends React.Component {
     render() {
         return (
             <div>
                 <h4>Selector</h4>
-                <TestForm />
+                <CheckboxGroup options={FORM_DATA.groups[0].list} name="box" />
             </div>
         );
     }
 }
+
+export default reduxForm({form: 'myForm'})(Selector);
---
+++ src/pages/selector/test-data.js
@@ -0,0 +1,24 @@
+export const FORM_DATA = {
+    groups: [
+        {
+            info: {
+                id: 32,
+                name: 'group 1'
+            },
+            list: [
+                {
+                    itemID: 100,
+                    label: 'name 1',
+                    value: '40',
+                    featured: true
+                },
+                {
+                    itemID: 200,
+                    label: 'name 2',
+                    value: '60',
+                    featured: true
+                }
+            ]
+        }
+    ]
+};
