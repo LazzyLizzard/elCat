b6a1da4f1baacd42a6ab8f2cbaf02d5e410fc2d8
git <lazy.lizard@yandex.ru>
2017-11-23 00:47:09 +0300

    MSBG-92: scss -> css ExtractTextPlugin added, css is 404

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import styles from './home.scss';
 
 export class Home extends Component {
     render() {
+        console.log(styles);
         return (
             <div>
                 <h4>home</h4>
-                <div><Link to="/fiche">fiche</Link></div>
+                <div>
+                    <Link to="/fiche">fiche</Link>
+                </div>
             </div>
         );
     }
