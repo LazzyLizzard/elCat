e7a6c631f69e54554956f4da7e3c62a8cf37bbb6
SF <lazy.lizard@yandex.ru>
2017-12-27 19:53:08 +0300

    MSBG-89: FE Develop form and parse query string
    * UNSTABLE filtering of submit result

--- src/pages/pick/show-results.js
+++ src/pages/pick/show-results.js
@@ -1,16 +1,25 @@
 import 'whatwg-fetch';
+import {isNil, assign} from 'lodash';
 import {getRequestEnvironment} from 'utils/get-request-environment';
 import {REMOTE_HTTPS} from 'contants/server-request-environment';
 import {ENDPOINT_PICK} from 'contants/end-points';
 
 const baseUrl = `${getRequestEnvironment(REMOTE_HTTPS)}${ENDPOINT_PICK}`;
 
-export const showResults = (result) => {
-    console.log(result); // eslint-disable-line no-console
+// TODO [sf] 27.12.2017 apply this
+// https://github.com/gaearon/redux-thunk
+export const showResults = (formSubmitResult, page = 1) => {
+    console.log(formSubmitResult); // eslint-disable-line no-console
+    console.log(page); // eslint-disable-line no-console
+    let box = [];
+    box = formSubmitResult.box.map((value, index) => (!isNil(value) && index));
+
+    console.log(box);
 
     fetch(baseUrl, {
         method: 'post',
-        body: result
+        // TODO [sf] 27.12.2017 filter !== null
+        body: JSON.stringify(assign({}, formSubmitResult, box))
     })
         .then(response => response.json())
         .then((json) => {
