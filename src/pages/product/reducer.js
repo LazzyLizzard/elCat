7ef2ffa0aff2287e341a568ae44dc9f84998d87c
lazy.lizard <lazy.lizard@yandex.ru>
2018-06-25 18:16:46 +0300

    MSBG-102: product
    * loader in product

--- src/pages/product/reducer.js
+++ src/pages/product/reducer.js
@@ -2,12 +2,14 @@ import {PRODUCT_STATE} from 'data-srtuctures/product';
 
 export const NAMESPACE = 'product';
 
+export const PRODUCT_FETCH_STARTED = 'PRODUCT/FETCH_STARTED';
 export const PRODUCT_FETCH_SUCCESS = 'PRODUCT/FETCH_SUCCESS';
 export const PRODUCT_FETCH_ERROR = 'PRODUCT/FETCH_ERROR';
 export const PRODUCT_CLEAR = 'PRODUCT/CLEAR';
 
 export const productReducer = (state = PRODUCT_STATE, action) => {
     switch (action.type) {
+        case PRODUCT_FETCH_STARTED:
         case PRODUCT_FETCH_SUCCESS:
         case PRODUCT_FETCH_ERROR:
         case PRODUCT_CLEAR:
