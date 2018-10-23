017b30fffd6d3b22a0b3a479dcd66829d4b0eb32
SF <lazy.lizard@yandex.ru>
2018-10-23 18:09:09 +0300

    _WP: moving to new webpack
    * to code review

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-filename-extension */
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {configureStore} from './store/configureStore';
+import {Routes} from './routes';
+import storeStructure from './store/storeStructure';
+import {rootReducer} from './reducers';
+
+// TODO [sf] 05.09.2017 rewrite completely
+const store = createStore(
+    rootReducer,
+    configureStore(storeStructure)
+);
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <Router
+                history={history}
+                routes={Routes}
+            />
+        </Provider>
+    ),
+    document.getElementById('root')
+);
+
